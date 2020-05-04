b2a1076013d2d4a47ff190d1b7313379f19d8d2a
Victor Paleologue <victor.paleologue@softbankrobotics.com>
2020-05-04 10:57:40 +0200

    Fast Downward: automatic test can be used as a performance test

--- app/src/main/cpp/test_native-lib.cpp
+++ app/src/main/cpp/test_native-lib.cpp
@@ -15,13 +15,20 @@ using JNIEnvInit = void;
 int main(int argc, char** argv) {
 
     // Checking arguments.
-    if (argc != 3) {
-        std::cout << "Usage: " << argv[0] << " PYTHONPATH PDDL" << std::endl;
+    if (argc < 3 || argc > 4) {
+        std::cout << "Usage: " << argv[0] << " PYTHONPATH PDDL_FILE [PROFILING]" << std::endl;
         throw std::invalid_argument("incorrect number of arguments");
     }
     auto pythonpath = argv[1];
-    std::cout << "PYTHONPATH=" << pythonpath << std::endl;
     auto pddl_path = argv[2];
+    bool is_profiling = false;
+    if (argc == 4) {
+      is_profiling = atoi(argv[3]);
+    }
+
+#define LOG if (!is_profiling) std::cout
+
+    LOG << "PYTHONPATH=" << pythonpath << std::endl;
 
     // Initialization of Java.
     // From https://docs.oracle.com/javase/7/docs/technotes/guides/jni/spec/invocation.html
@@ -45,10 +52,12 @@ int main(int argc, char** argv) {
             env, nullptr, env->NewStringUTF(pythonpath));
 
     const char* effective_pythonpath = env->GetStringUTFChars(j_effective_pythonpath, 0);
-    std::cout << "Effective PYTHONPATHs=" << effective_pythonpath << std::endl;
+    LOG << "Effective PYTHONPATHs=" << effective_pythonpath << std::endl;
 
     if (!strstr(effective_pythonpath, pythonpath))
         throw std::runtime_error("discrepancy of proposed and effective PYTHONPATHs");
+
+    env->ReleaseStringUTFChars(j_effective_pythonpath, effective_pythonpath);
 
     // Extracting the PDDL from the provided file.
     std::ifstream pddl_file;
@@ -56,30 +65,38 @@ int main(int argc, char** argv) {
     std::stringstream pddl_ss;
     pddl_ss << pddl_file.rdbuf();
     auto pddl = pddl_ss.str();
-    std::cout << "PDDL:" << std::endl << pddl << std::endl;
+    LOG << "PDDL:" << std::endl << pddl << std::endl;
 
     // The file should exhibit a domain, and then a problem.
     size_t split_pos = pddl.find("(define (problem");
     std::string domain = pddl.substr(0, split_pos);
     std::string problem = pddl.substr(split_pos);
 
-    // Searching for a plan.
-    std::cout << "Domain:" << std::endl << domain << std::endl;
-    std::cout << "Problem:" << std::endl << problem << std::endl;
-    auto sas = Java_com_softbankrobotics_pddlsandbox_PlanningKt_translatePDDLToSAS(
-            env, nullptr, env->NewStringUTF(domain.c_str()), env->NewStringUTF(problem.c_str()));
-    if (!sas) {
-        throw std::runtime_error("Failed to translate PDDL to SAS");
+    unsigned repeats = 1;
+    if (is_profiling)
+      repeats = 100;
+
+    for (unsigned i = 0; i < repeats; ++i) {
+      // Searching for a plan.
+      LOG << "Domain:" << std::endl << domain << std::endl;
+      LOG << "Problem:" << std::endl << problem << std::endl;
+      auto j_sas = Java_com_softbankrobotics_pddlsandbox_PlanningKt_translatePDDLToSAS(
+              env, nullptr, env->NewStringUTF(domain.c_str()), env->NewStringUTF(problem.c_str()));
+      if (!j_sas) {
+          throw std::runtime_error("Failed to translate PDDL to SAS");
+      }
+      auto c_sas = env->GetStringUTFChars(j_sas, nullptr);
+      LOG << "SAS:" << std::endl << c_sas << std::endl;
+
+      auto j_plan = Java_com_softbankrobotics_pddlsandbox_PlanningKt_searchPlanFromSAS(
+              env, nullptr, j_sas, env->NewStringUTF("astar(add())"));
+      auto c_plan = env->GetStringUTFChars(j_plan, nullptr);
+      LOG << "Plan:" << std::endl << env->GetStringUTFChars(j_plan, nullptr) << std::endl;
+
+      // Clean-up.
+      env->ReleaseStringUTFChars(j_plan, c_plan);
+      env->ReleaseStringUTFChars(j_sas, c_sas);
     }
-    std::cout << "SAS:" << std::endl << env->GetStringUTFChars(sas, nullptr) << std::endl;
-
-    auto plan = Java_com_softbankrobotics_pddlsandbox_PlanningKt_searchPlanFromSAS(
-            env, nullptr, sas, env->NewStringUTF("astar(add())"));
-    std::cout << "Plan:" << std::endl << env->GetStringUTFChars(plan, nullptr) << std::endl;
-
-    // Clean-up.
-    env->ReleaseStringUTFChars(sas, env->GetStringUTFChars(sas, nullptr));
-    env->ReleaseStringUTFChars(plan, env->GetStringUTFChars(plan, nullptr));
 
     jvm->DestroyJavaVM();
     return EXIT_SUCCESS;
