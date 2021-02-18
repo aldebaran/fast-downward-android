3d031b02e9e68a7326684bca7c811bc677c7fd2a
Victor Paleologue <victor.paleologue@softbankrobotics.com>
2021-02-18 09:31:09 +0100

    Version 2.1.1

--- jenkins-continuous.groovy
+++ jenkins-continuous.groovy
@@ -1,12 +1,35 @@
 @Library('pipeline-library') _
 
+abis = ['armeabi-v7a']
+
+// Sets up the environment to build Android projects.
 node("android-build-jdk8") {
 
     stage('Checkout SCM') { checkout scm }
 
-    stage('Compile') {
-        sh "./gradlew assembleRelease -PABIS=\"armeabi-v7a\""
+    stage('Compile Library') {
+        sh './gradlew :library:assembleRelease'
+        archiveArtifacts '**/*.aar'
     }
 
-    stage('Archive AAR') { archiveArtifacts '**/*.aar,**/*.apk' }
+    stage('Compile Application') {
+        withCredentials([
+                file(credentialsId: 'keystoreFileAndroid', variable: 'KEYSTORE_FILE'),
+                string(credentialsId: 'keystorePasswordAndroid', variable: 'KEYSTORE_PASSWORD'),
+                string(credentialsId: 'keystoreAliasSample', variable: 'KEYSTORE_ALIAS'),
+                string(credentialsId: 'aliasPasswordSample', variable: 'ALIAS_PASSWORD'),
+        ]) {
+            for (abi in abis) {
+                echo "Compiling app for ($abi)..."
+                sh "./gradlew :app:assembleRelease -PABIS=$abi"
+
+                // KLUDGE: by building it twice, we make sure the asset python-modules.zip is found
+                echo "Recompiling app for ($abi)..."
+                sh "./gradlew :app:assembleRelease -PABIS=$abi"
+
+                echo "Archiving app for ($abi)..."
+                archiveArtifacts "**/*-$abi-*.apk"
+            }
+        }
+    }
 }
--- jenkins-release.groovy
+++ jenkins-release.groovy
@@ -1,43 +1,47 @@
-@Library('pipeline-library')
+@Library('pipeline-library') _
 
-extraBuildOptions = "" // ABIs can be specified using \"-PABIS=...;...\"
+abis = ['armeabi-v7a', 'x86', 'x86_64']
 
 // Sets up the environment to build Android projects.
 node("android-build-jdk8") {
 
     stage('Checkout SCM') { checkout scm }
 
-    stage('Compile') {
+    stage('Compile Library') {
+        sh './gradlew :library:assembleRelease'
+        archiveArtifacts '**/*.aar'
+        withCredentials([
+                usernamePassword(credentialsId: 'nexusDeployerAccount',
+                        passwordVariable: 'NEXUS_PASSWORD',
+                        usernameVariable: 'NEXUS_USER')
+        ]) {
+            if (env.BRANCH_NAME == "develop") {
+                BUILD_TYPE = "SNAPSHOT"
+            } else {
+                BUILD_TYPE = "RELEASE"
+            }
+            sh "./gradlew -DNEXUS_PASSWORD=$NEXUS_PASSWORD -DNEXUS_USER=$NEXUS_USER -DBUILD=$BUILD_TYPE :library:uploadArchives"
+        }
+    }
+
+    stage('Compile Application') {
         withCredentials([
                 file(credentialsId: 'keystoreFileAndroid', variable: 'KEYSTORE_FILE'),
                 string(credentialsId: 'keystorePasswordAndroid', variable: 'KEYSTORE_PASSWORD'),
                 string(credentialsId: 'keystoreAliasSample', variable: 'KEYSTORE_ALIAS'),
                 string(credentialsId: 'aliasPasswordSample', variable: 'ALIAS_PASSWORD'),
         ]) {
-            sh "./gradlew assembleRelease $extraBuildOptions"
-        }
-    }
+            for (abi in abis) {
+                echo "Compiling app for ($abi)..."
+                sh "./gradlew :app:assembleRelease -PABIS=$abi"
 
-    stage('Upload AAR to Nexus') {
-        if (env.BRANCH_NAME == "develop") {
-            BUILD_TYPE = "SNAPSHOT"
-        } else {
-            BUILD_TYPE = "RELEASE"
-        }
-        echo "BUILD_TYPE=$BUILD_TYPE"
+                // KLUDGE: by building it twice, we make sure the asset python-modules.zip is found
+                echo "Recompiling app for ($abi)..."
+                sh "./gradlew :app:assembleRelease -PABIS=$abi"
 
-        echo 'Entering credentials context...'
-        withCredentials([
-                usernamePassword(credentialsId: 'nexusDeployerAccount',
-                        passwordVariable: 'NEXUS_PASSWORD',
-                        usernameVariable: 'NEXUS_USER')
-        ]) {
-            echo 'Now in credentials context.'
-            sh "./gradlew -DNEXUS_PASSWORD=$NEXUS_PASSWORD -DNEXUS_USER=$NEXUS_USER -DBUILD=$BUILD_TYPE :library:uploadArchives $extraBuildOptions"
+                echo "Archiving app for ($abi)..."
+                archiveArtifacts "**/*-$abi-*.apk"
+            }
         }
-    }
-
-    stage('Archive AAR') {
-        archiveArtifacts '**/*.aar,**/*.apk'
     }
 }
